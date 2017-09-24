3e35e95abb8c6803cf55d46e2274ac36f5d9aca9
sgorka <30950966+sgorka@users.noreply.github.com>
2017-09-24 11:10:44 +0200

    Add function get_nationalparty

---
+++ R/get_nationalparty.R
@@ -0,0 +1,23 @@
+#' Get data about all deputies
+#'
+#' @export
+#' @import magrittr
+#'
+#'
+
+
+get_nationalparty <- function(deputy_id, date = Sys.Date()) {
+
+  stopifnot(is.character(deputy_id))
+
+  if(!exists("national_party")) {
+    stop("Please load national parties database first.")
+  }
+  national_party <- national_party_P8[which(national_party_P8$date_beginning <= date &
+                                  ifelse(is.na(national_party_P8$date_end),TRUE, national_party_P8$date_end >= date) &
+                                    national_party_P8$ID_deputy==deputy_id
+  ),c("name")]
+
+  return(national_party)
+
+}
